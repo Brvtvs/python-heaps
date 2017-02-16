a70da57e71e44479e8540158a3db877a4704c184
Conrad S <cdstosz@gmail.com>
2017-02-16 10:59:00 -0500

    Replace binary heap with a d-ary heap.

--- d_heap.py
+++ d_heap.py
@@ -2,24 +2,29 @@
 import unittest
 
 
-# An implementation of a binary heap using a python list as an array.
-class BinaryHeap:
+# An implementation of a d-ary heap (aka d-way heap or d-heap) with an implicit array-based structure, using a python
+# list as an array.
+#
+# A d-ary heap is a binary heap, but with an arbitrary branching factor.
+class DHeap:
 
     @staticmethod
-    def heapify(unordered_list, max_heap=True):
+    def heapify(unordered_list, max_heap=True, branching_factor=2):
         """
         Takes an existing list and turns it into a heap.
         Completes in O(n log n) time.
         :param unordered_list: The array to heapify.
         :param max_heap: True to create a max heap, else False to create a min heap.
+        :param branching_factor: The maximum number of children each parent in the tree should have. Defaults to 2 for
+        a binary tree.
         :return: A heap wrapping the given list, after it has heapified the list.
         """
-        heap = BinaryHeap(max_heap=max_heap)
+        heap = DHeap(max_heap=max_heap, branching_factor=branching_factor)
         heap.heap_array = unordered_list
         heap.last_index = len(unordered_list) - 1
 
         # Starts at the parent of the heap's last node.
-        start = (heap.last_index - 1) // 2
+        start = (heap.last_index - 1) // heap.branching_factor
 
         # Sifts down each parent in the tree until all parents in the tree have been made to satisfy the heap property.
         while start >= 0:
@@ -36,7 +41,7 @@ class BinaryHeap:
         :param ascending: True to sort into ascending order, else False to order in descending order.
         """
         # Step 1: turns the list into a heap.
-        heap = BinaryHeap.heapify(unordered_list, max_heap=ascending)
+        heap = DHeap.heapify(unordered_list, max_heap=ascending)
 
         # Step 2: Pops each element off the heap, but keeps them in the list, ordered, rather than removing them.
         while heap.last_index >= 0:
@@ -47,13 +52,16 @@ class BinaryHeap:
             # recursively heapifies the new top element downwards until its in its correct position.
             heap.__sift_down(0)
 
-    def __init__(self, initial_contents=(), max_heap=True):
+    def __init__(self, initial_contents=(), max_heap=True, branching_factor=2):
         """
         Class constructor. Completes in O(n log n) time.
         :param initial_contents: The contents to initially store in the heap. Copies the given values and does not edit
         the passed in list.
         :param max_heap: True to create a max heap, else False to create a min heap.
+        :param branching_factor: The maximum number of children each parent in the tree should have. Defaults to 2 for
+
         """
+        self.branching_factor = branching_factor
         self.max_heap = max_heap
         self.heap_array = []
         self.last_index = -1
@@ -129,7 +137,7 @@ class BinaryHeap:
         until it finds a valid place.
         :param index: The index to start at.
         """
-        parent = (index - 1) // 2
+        parent = (index - 1) // self.branching_factor
         if parent >= 0 and self.__compare(index, parent):
             self.__swap(index, parent)
             self.__sift_up(parent)
@@ -140,17 +148,20 @@ class BinaryHeap:
         until it finds a valid place.
         :param index: The index to start with.
         """
-        left = (index * 2) + 1
-        right = (index * 2) + 2
         new_parent = index  # largest defaults to parent, then tested against the children's values
 
-        # if the left child exists and is better than the parent, marked as the new parent
-        if left <= self.last_index and self.__compare(left, new_parent):
-            new_parent = left
+        # finds the largest node among the parent and its children
+        i = 1
+        while i <= self.branching_factor:
 
-        # if the right child exists and is better than the parent and the left child, marked than as the new parent
-        if right <= self.last_index and self.__compare(right, new_parent):
-            new_parent = right
+            child = (index * self.branching_factor) + i
+
+            # if the child exists and is better than the parent and any other children checked so far, marked as the
+            # new parent
+            if child <= self.last_index and self.__compare(child, new_parent):
+                new_parent = child
+
+            i += 1
 
         # if the new parent differs from the old parent, swap the two and continue recursively heapifying
         if new_parent is not index:
@@ -164,11 +175,11 @@ class BinaryHeap:
          heap.
         :return: True if the tree including this parent and all its children is valid, Else False.
         """
-        first_child = (parent * 2) + 1
+        first_child = (parent * self.branching_factor) + 1
 
         # Checks each child against its parent, and recursively checks any further descendants against their parents.
         i = 0
-        while i < 2:
+        while i < self.branching_factor:
             child = first_child + i
             if child <= self.last_index:
                 # Base case: parent and child are out of order.
@@ -192,31 +203,38 @@ class BinaryHeap:
         return self.last_index + 1
 
 
-# Unit tests for BinaryHeap
-class BinaryHeapTest(unittest.TestCase):
+# Unit tests for DHeap
+class DHeapTest(unittest.TestCase):
 
-    def test_heap_construction(self):
-        """Tests creating a heap from an existing list without modifying the passed in list."""
+    def test_binary_heap_construction(self):
+        """Tests creating a binary heap from an existing list without modifying the passed in list."""
         source = [1, 1, 5, 10, -23, 105]
-        heap = BinaryHeap(source)
+        heap = DHeap(source)
         self.assertEqual(heap.validate_heap(), True)
         self.assertListEqual(source, [1, 1, 5, 10, -23, 105])
+
+    def test_ternary_heap_construction(self):
+        """Tests creating a ternary heap from an existing list without modifying the passed in list."""
+        source = [1, 1, 5, 10, -23, 105, 1200412, -2352, 0, 0, 101, 45, -2]
+        heap = DHeap(source, branching_factor=3)
+        self.assertEqual(heap.validate_heap(), True)
+        self.assertListEqual(source, [1, 1, 5, 10, -23, 105, 1200412, -2352, 0, 0, 101, 45, -2])
 
     def test_heapify(self):
         """Tests turning an existing list into a heap."""
         source = [1, 0, 5, 103, -23, -2213415]
-        heap = BinaryHeap.heapify(source)
+        heap = DHeap.heapify(source)
         self.assertEqual(heap.validate_heap(), True)
 
     def test_heapsort(self):
-        """Tests the heapsort implementation using a BinaryHeap."""
+        """Tests the heapsort implementation using a DHeap."""
         unordered = [1, 5, 64, 64, 12315, 677, -15, -1002]
-        BinaryHeap.heapsort(unordered, True)
+        DHeap.heapsort(unordered, True)
         self.assertListEqual(unordered, [-1002, -15, 1, 5, 64, 64, 677, 12315])
 
     def test_push(self):
         """Tests pushing values onto a heap."""
-        heap = BinaryHeap()
+        heap = DHeap()
         heap.push(1)
         self.assertEqual(heap.validate_heap(), True)
         heap.push(0)
@@ -230,7 +248,7 @@ class BinaryHeapTest(unittest.TestCase):
 
     def test_peek(self):
         """Tests peeking the top value from the heap."""
-        heap = BinaryHeap([1, 1, 5, 10, -23, 105])
+        heap = DHeap([1, 1, 5, 10, -23, 105])
         self.assertEqual(heap.validate_heap(), True)
         self.assertEqual(heap.peek(), 105)
         heap.push(1000)
@@ -238,7 +256,7 @@ class BinaryHeapTest(unittest.TestCase):
 
     def test_pop(self):
         """Tests popping values off of the heap."""
-        heap = BinaryHeap([-10, 0, 5, -2, 5, 1024, -999])
+        heap = DHeap([-10, 0, 5, -2, 5, 1024, -999])
         self.assertEqual(heap.validate_heap(), True)
         self.assertEqual(heap.pop(), 1024)
         self.assertEqual(heap.validate_heap(), True)
@@ -252,7 +270,7 @@ class BinaryHeapTest(unittest.TestCase):
 
     def test_min_heap(self):
         """Tests using a min heap rather than the default max heap."""
-        heap = BinaryHeap([-10, 0, 5, -2, 5, 1024, -999])
+        heap = DHeap([-10, 0, 5, -2, 5, 1024, -999])
         self.assertEqual(heap.validate_heap(), True)
         heap.push(12)
         self.assertEqual(heap.validate_heap(), True)
